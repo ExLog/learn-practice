1fb7a3f45374395e45f9dbf9f1b18084abb3378c
Muhammad Dony Mulya <55097092+ExLog@users.noreply.github.com>
2021-06-05 20:59:05 +0800

    ?

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -6,6 +6,8 @@ fn main() {
     println!("Guess the number!");
 
     // Generating random number
+    let mut guessed: u32 = 0;
+
     let secret_number = rand::thread_rng().gen_range(1..101);
 
     loop {
@@ -19,12 +21,16 @@ fn main() {
         let guess: u32 = guess.trim().parse().expect("Please input a number");
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
+            Ordering::Less => {
+                println!("Too small!")
+            }
             Ordering::Greater => println!("Too Big!"),
             Ordering::Equal => {
                 println!("You win!");
                 break;
             }
         }
+        guessed += 1;
     }
+    println!("You guessed {} times!", guessed)
 }
---
+++ rust/variables/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // const x = 5 by default
+    let x = 5;
+    println!("The value of x is : {}", x);
+    // Error cuz x is immutable by default
+    // immutable: the value can't be mutated / nilainya tidak bisa berubah
+    // x = 6;
+    println!("The value of x is : {}", x);
+}
