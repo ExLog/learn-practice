4493d20e7c8ad43cb834fe73a4415e7839788969
Veirt <55097092+Veirt@users.noreply.github.com>
2023-07-26 22:46:29 +0800

    leetcode: 412

---
+++ leetcode/412/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from typing import List
+
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        ans = []
+        for i in range(1, n + 1):
+            if i % 15 == 0:
+                ans.append("FizzBuzz")
+            elif i % 3 == 0:
+                ans.append("Fizz")
+            elif i % 5 == 0:
+                ans.append("Buzz")
+            else:
+                ans.append(str(i))
+
+        return ans
+
+
+    def __init__(self) -> None:
+        print(self.fizzBuzz(3))
+
+    
+
+Solution()
