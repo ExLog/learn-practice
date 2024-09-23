c4ee29d1debe84e275a003d33e4077b7cd7c415a
Veirt <veirt@duck.com>
2024-09-23 20:18:05 +0800

    leetcode: 2 - two sum

---
+++ leetcode/02/main.js
@@ -0,0 +1,25 @@
+/**
+ Bruteforce
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (i === j) {
+        continue;
+      }
+
+      const sumResult = nums[i] + nums[j];
+      if (sumResult === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+// console.log(twoSum([2, 7, 11, 15], 9));
+// console.log(twoSum([3, 2, 4], 6));
+// console.log(twoSum([3, 3], 6));
+// console.log(twoSum([-3, 4, 3, 90], 0));
